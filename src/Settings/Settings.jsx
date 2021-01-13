46d3c73c63f75e71385ecab55efe41c2e6c4d064
Davit Bagratuni <77169499+Bagratuni94@users.noreply.github.com>
2021-01-13 19:49:13 +0400

    Add files via upload

---
+++ src/Settings/Settings.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Settings.module.css'
+const Settings = (props) => {
+    return(
+        <div>
+            Settingsssss
+        </div>
+    )
+}
+
+export default Settings;
